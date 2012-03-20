22d50804695ce8310a5fbcde2490823fcc9f9412
Andreas Wassmer <a.wassmer@unibas.ch>
2012-03-20 08:43:04 +0100

    Added jquery_includes.php which generates includes via php hiding private lines.

---
+++ jquery_includes.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<link rel='stylesheet' href='http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css'/>" . chr(10);
+  echo "<script type='text/javascript' src='http://code.jquery.com/jquery-1.7.1.min.js'></script>" .chr(10);
+	echo "<script src='http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js'></script>" . chr(10);	
+
+// use this for local debugging
+//	echo "<link rel='stylesheet' href='http://localhost/~wassmer/jquery/jquery.mobile-1.0a1.min.css'/>" . chr(10);
+//  echo "<script type='text/javascript' src='http://localhost/~wassmer/jquery/jquery-1.6.1.js'></script>" . chr(10);
+//	echo "<script type='text/javascript' src='http://localhost/~wassmer/jquery/jquery.mobile.1.0a4.1.js'></script>" . chr(10);
+?>
